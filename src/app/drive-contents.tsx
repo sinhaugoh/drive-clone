cbd7bafc366fb46ce5e1258f87cddabdaf26a2c8
sinhaugoh <sinhaugoh456@hotmail.com>
2025-08-30 18:01:17 +0800

    breadcrumb fix

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -10,8 +10,8 @@ import Link from "next/link";
 export default function DriveContents(props: {
   files: (typeof files.$inferSelect)[];
   folders: (typeof folders.$inferSelect)[];
+  parents: (typeof folders.$inferSelect)[];
 }) {
-  const breadcrumbs: unknown[] = [];
   const handleUpload = () => {
     alert("Upload functionality would be implemented here");
   };
@@ -24,7 +24,7 @@ export default function DriveContents(props: {
             <Link href="/f/1" className="mr-2 text-gray-300 hover:text-white">
               My Drive
             </Link>
-            {breadcrumbs.map((folder, index) => (
+            {props.parents.map((folder, index) => (
               <div key={folder.id} className="flex items-center">
                 <ChevronRight className="mx-2 text-gray-500" size={16} />
                 <Link
